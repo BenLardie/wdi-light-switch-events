f50e4c51d654bff0a377eeb8f72dd6affbb88e17
Ben Lardie <benlardie@gmail.com>
2019-03-27 14:26:39 -0400

    light switch event is working

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() { 
 
+let text = document.getElementsByClassName('status');
+
+text = document.querySelector('body > h1');
+text.innerText = "let's flip this switch";
+
+let body = document.querySelector('body')
+
+let lightSwitch = document.getElementsByClassName('switch on')[0];
+
+lightSwitch.addEventListener('click', function() {
+    alert('OH NO YOU FLIPPED THE SWITCH!');
+    body.classList.add('dark');
+});
+
+
+
+
+
+
+
+
+
+
+
+});
 
