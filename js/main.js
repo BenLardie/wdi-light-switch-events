5fac4de82f308b5ed2407fdb4e83ee6b77fe397d
Ben Lardie <benlardie@gmail.com>
2019-03-27 14:43:37 -0400

    all finished

--- js/main.js
+++ js/main.js
@@ -3,15 +3,20 @@ document.addEventListener('DOMContentLoaded', function() {
 let text = document.getElementsByClassName('status');
 
 text = document.querySelector('body > h1');
-text.innerText = "let's flip this switch";
+
 
 let body = document.querySelector('body')
 
 let lightSwitch = document.getElementsByClassName('switch on')[0];
 
 lightSwitch.addEventListener('click', function() {
-    alert('OH NO YOU FLIPPED THE SWITCH!');
-    body.classList.add('dark');
+    body.classList.toggle('dark');
+    lightSwitch.classList.toggle('off');
+    if (text.innerText == "It's so bright in here!") {
+        text.innerText = 'Who turned off the lights';
+    } else {
+        text.innerText = "It's so bright in here!"
+    }
 });
 
 
